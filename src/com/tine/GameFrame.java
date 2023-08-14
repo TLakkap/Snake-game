a24ff20c584f07f07d31ec157de50dddf5a6c76d
Tiina Lakkapää <t0lati02@students.oamk.fi>
2023-08-14 19:42:13 +0300

    init

---
+++ src/com/tine/GameFrame.java
@@ -0,0 +1,18 @@
+package com.tine;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+    GameFrame(){
+
+        //GamePanel panel = new GamePanel();
+        //Shorter version of the above since the "panel" is not used anywhere
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null); //appears in the middle of the screen
+    }
+}
---
+++ src/com/tine/Main.java
@@ -0,0 +1,11 @@
+package com.tine;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //GameFrame frame = new GameFrame();
+        //Shorter version of the above since the "frame" is not used anywhere
+        new GameFrame();
+    }
+}
