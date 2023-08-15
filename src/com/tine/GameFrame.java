94e553dfd82839c40e9af6aa69f7c733152cea2c
Tiina Lakkapää <t0lati02@students.oamk.fi>
2023-08-15 21:26:34 +0300

    Gamepanel constructor

--- src/com/tine/GameFrame.java
+++ src/com/tine/GameFrame.java
@@ -8,6 +8,7 @@ public class GameFrame extends JFrame {
         //GamePanel panel = new GamePanel();
         //Shorter version of the above since the "panel" is not used anywhere
         this.add(new GamePanel());
+
         this.setTitle("Snake");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
